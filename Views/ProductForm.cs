aff2d8b2c7e41225269837f9987b073cb97a9fd8
Aswin <agladsingh@gmail.com>
2024-06-24 11:59:03 +0530

    first commit

---
+++ Views/ProductForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoppingCartApp.Views
+{
+    public partial class ProductForm : Form
+    {
+        public ProductForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
